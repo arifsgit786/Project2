b396ea7225799bd0545323acfe493c525a1d9094
shaik mohammad arif <arifbuddy39@gmail.com>
2024-03-28 22:20:17 -0500

    added Contact list component

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
 import FancyCard from './components/FancyCard'
 import ActionCard from './components/ActionCard'
+import ContactList from './components/ContactList'
 ///import ActionCard from './components/ActionCard'
 //import ContactList from './components/ContactList'
 
@@ -14,6 +15,7 @@ const App = () => {
         <FlatCards />
         <ElevatedCards/>
         <FancyCard />
+        <ContactList/>
         <ActionCard/>
       </ScrollView>
     </SafeAreaView>
